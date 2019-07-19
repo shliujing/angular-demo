3b6c317295b8377b0cf609da77e316ad44f7f0d6
lj <i@iamlj.com>
2019-07-19 17:35:05 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
+/*这里是从Angular核心模块里面引入了component装饰器*/
 import { Component } from '@angular/core';
 
+/*用装饰器定义了一个组件以及组件的元数据  所有的组件都必须使用这个装饰器来注解*/
 @Component({
+  /*组件元数据  Angular会通过这里面的属性来渲染组件并执行逻辑
+  * selector就是css选择器，表示这个组件可以通过app-root来调用，index.html中有个<app-root>Loading...</app-root>标签，这个标签用来展示该组件的内容
+  *templateUrl  组件的模板，定义了组件的布局和内容
+  *styleUrls   该模板引用那个css样式
+  * */
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
